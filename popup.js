21d38b351c61a5d73f0ce91651976eb364d5cca0
Kevin Schmauss <k.schmauss@fis-gmbh.de>
2020-06-08 09:22:21 +0200

    initial

---
+++ popup.js
@@ -0,0 +1,46 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    var checkbox = document.querySelector('#isActiveSwitch')
+
+    chrome.storage.sync.get(['isActive'], function (data) {
+        var isActive = data.isActive
+        setInitialState(isActive, checkbox)
+    })
+
+    checkbox.addEventListener('change', function () {
+        if (this.checked) {
+            isActive = 'true'
+            chrome.storage.sync.set({ 'isActive': 'true' })
+            sendState(isActive)
+        } else {
+            isActive = 'false'
+            chrome.storage.sync.set({ 'isActive': 'false' })
+            sendState(isActive)
+        }
+    })
+}, false)
+
+function setInitialState(isActive, checkbox) {
+    if (isActive === undefined) {
+        isActive = 'true'
+        chrome.storage.sync.set({ 'isActive': 'true' })
+    } else {
+        if (isActive == 'true') {
+            checkbox.checked = true
+        } else if (isActive == 'false') {
+            checkbox.checked = false
+        }
+    }
+}
+
+function sendState(state) {
+    chrome.tabs.query({url: 'https://*.hana.ondemand.com/itspaces/*'},
+        function (tabs) {
+            for(var i = 0; i < tabs.length; i++) {
+                chrome.tabs.sendMessage(tabs[i].id, { 'isActive': state }, function(response) {
+                    console.log(response)
+                })
+            }
+        }
+    )
+}
